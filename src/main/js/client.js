2276a6df671a4b6e321ca045480e494b7bb6fd18
Purba <richard.purba@accenture.com>
2019-03-05 16:36:08 +0100

    comment to client.js of what it is

--- src/main/js/client.js
+++ src/main/js/client.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// this fileis custom code that configures rest.js to 
+//include support for HAL, URI Templates, and other things. It also sets the default Accept request header to application/hal+json
 
 const rest = require('rest');
 const defaultRequest = require('rest/interceptor/defaultRequest');
