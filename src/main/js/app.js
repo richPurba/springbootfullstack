81047a1fdf1c5bf422aedb9c4719659363e5f879
Purba <richard.purba@accenture.com>
2019-03-05 16:29:07 +0100

    adding package.json and app.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,3 +1,22 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 const client = require('./client');
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {employee:[]}
+    }
+
+    componentDidAmount(){
+        client({method:'GET',path:'/api/employees'}).done(
+            response => {this.setState({employees:response.entity._embedded.employees})}
+        );
+    }
+
+    render(){
+        return(
+            <EmployeeList employees = {this.state.employees}/>
+        )
+    }
+}
