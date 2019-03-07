078ff5c2f379f6a163aadaff38ced356a3c3f2f4
RichardPurba <tepetrol@yahoo.com>
2019-03-07 16:39:23 +0100

    adding webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 var path = require('path');
 
 module.exports = {
-	entry: './src/main/js/app.js',
+	entry: './src/main/js/app.js', // an entry point of to begin building its internal dependency graph
 	devtool: 'sourcemaps',
 	cache: true,
 	mode: 'development',
 	output: {
 		path: __dirname,
-		filename: './src/main/resources/static/built/bundle.js'
+		filename: './src/main/resources/static/built/bundle.js' //where to emit the bundles it creates and how to name them
 	},
 	module: {
 		rules: [
 			{
-				test: path.join(__dirname, '.'),
-				exclude: /(node_modules)/,
-				use: [{
+				test: path.join(__dirname, '.'),//which file should be transformed in the require()/import statement! Transform to what? WHy transform?
+				exclude: /(node_modules)/,//excluding node_modules
+				use: [{// which loader should be used to do the transforming
 					loader: 'babel-loader',
 					options: {
 						presets: ["@babel/preset-env", "@babel/preset-react"]
