dd21d8eff1f63ca3e0862c1849c8399a9e242096
RichardPurba <tepetrol@yahoo.com>
2019-03-07 16:38:14 +0100

    Recovering database

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -10,15 +10,15 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository){
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository){
         return args -> {
             log.info("Preloading" + employeeRepository.save(new Employee("Bilbo","Baggins","chef")));
             log.info("Preloading" + employeeRepository.save(new Employee("Martina","Klaskova","wife")));
-            orderRepository.save(new Order("Macbook Pro",Status.COMPLETED));
-            orderRepository.save(new Order("iPhone",Status.IN_PROGRESS));
-            orderRepository.findAll().forEach(order -> {
-                log.info("Preloaded " + order);
-            });
+//            orderRepository.save(new Order("Macbook Pro",Status.COMPLETED));
+//            orderRepository.save(new Order("iPhone",Status.IN_PROGRESS));
+//            orderRepository.findAll().forEach(order -> {
+//                log.info("Preloaded " + order);
+//            });
         };
     }
 }
