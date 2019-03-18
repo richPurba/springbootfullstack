8590235a7eb5771566e021b9bcfb91f8003c7253
RichardPurba <tepetrol@yahoo.com>
2019-03-18 15:32:29 +0100

    reverting back the database state

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -15,7 +15,7 @@ public class LoadDatabase {
             log.info("Preloading" + employeeRepository.save(new Employee("Bilbo","Baggins","chef")));
             log.info("Preloading" + employeeRepository.save(new Employee("Martina","Klaskova","wife")));
             orderRepository.save(new Order("Macbook Pro",Status.COMPLETED));
-           // orderRepository.save(new Order("iPhone",Status.IN_PROGRESS));
+            orderRepository.save(new Order("iPhone",Status.IN_PROGRESS));
             orderRepository.findAll().forEach(order -> {
                 log.info("Preloaded " + order);
             });
