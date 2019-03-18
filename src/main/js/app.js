8e03c71a919814dff433fa76a983313de3516279
RichardPurba <tepetrol@yahoo.com>
2019-03-18 15:30:53 +0100

    finalizing app.js entry point for rest client api

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,4 +1,5 @@
 import '../resources/static/main.css'
+
 const React = require('react');
 const ReactDom = require('react-dom');
 const client = require('./client');
@@ -9,22 +10,37 @@ class App extends React.Component{
         this.state = {orders:[]};
     }
 
-    componentDidAmount(){
-        client({method:'GET',path:'/api/orders'}).done(
-            response => {this.setState({orders:response.entity._embedded.orders})}
-        );
+    componentDidMount(){
+        this.handleTheReturn(); // see this https://stackoverflow.com/questions/24842505/reactjs-unexpected-infinite-loop-with-render
+    }
+    handleTheReturn(){
+        fetch('http://localhost:8080/api/orders').then(response =>
+       { if(response.ok){
+        return response.json();
+    } else{
+        throw new Error(" end point is not reachable");
+    }}).then( 
+        data => this.setState({orders:data._embedded.orders})
+    )
     }
 
     render(){
+        console.log('from App state');
+        console.log(this.state);
         return(
-            <OrderList orders = {this.state.orders}/>
-        )
+            <OrderList orders = {this.state.orders} />
+        
+            )
     }
 }
 
 class OrderList extends React.Component{
     render(){
-        const orders = this.props.orders;
+        const orders = this.props.orders.map(order=>(
+            <Order key={order._links.self.href} order={order}/>
+        ));
+        console.log(orders);
+    
             return(
                 <table>
                     <tbody>
